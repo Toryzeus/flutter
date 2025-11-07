1c50515dfcad254a777615042adef24f9bfc5b2a
Toryzeus <toryzeus@gmail.com>
2025-11-07 16:28:53 +0100

    actualizando actividad 5 kyu8

--- bin/dartCodewars/A07_5.dart
+++
@@ -1,9 +0,0 @@
-/*I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
-
-P.S. Each array includes only integer numbers. Output is a number too.
-*/
-int arrayPlusArray(List<int> arr1, List<int> arr2) {
-  int sum1 = arr1.reduce((value, element) => value + element);//suma los elementos del primer array
-  int sum2 = arr2.reduce((value, element) => value + element);//suma los elementos del segundo array
-  return sum1 + sum2;//retorna la suma de las dos sumas
-}
