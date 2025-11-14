45e43531aa165567a4066b20c94c93a8ae8971f0
Toryzeus <toryzeus@gmail.com>
2025-11-14 15:37:15 +0100

    actualizar actividad 8

--- bin/dartCodewars/A07_8.dart
+++ bin/dartCodewars/A07_8.dart
@@ -6,13 +6,16 @@
 
 ¡Vamos a jugar! ¡Tienes que devolver el jugador que ganó! En caso de retorno del sorteo Draw!.*/
 String rps(String p1, String p2) {
-  if (p1 == p2) {//si ambos jugadores eligen lo mismo, es un empate
-    return "Empate!";
-  } else if ((p1 == "piedra" && p2 == "tijeras") ||//si el jugador 1 gana
-      (p1 == "tijeras" && p2 == "papel") ||//si el jugador 1 gana
-      (p1 == "papel" && p2 == "piedra")) {//si el jugador 1 gana
-    return "jugador 1 ganó!";
+  p1 = p1.trim().toLowerCase();//.trim() elimina espacios en blanco y toLowerCase() convierte a minusculas
+  p2 = p2.trim().toLowerCase();//.trim() elimina espacios en blanco y toLowerCase() convierte a minusculas
+
+  if (p1 == p2) {//si ambos jugadores eligen lo mismo
+    return "Draw!";
+  } else if ((p1 == "rock" && p2 == "scissors") ||//si el jugador 1 gana
+             (p1 == "scissors" && p2 == "paper") ||//si el jugador 1 gana
+             (p1 == "paper" && p2 == "rock")) {//si el jugador 1 gana 
+    return "Player 1 won!";
   } else {//si el jugador 2 gana
-    return "jugador 2 ganó!";
+    return "Player 2 won!";
   }
-} 
+}
